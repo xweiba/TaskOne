dbb219f71c719c249257e6a15f231c17a99c97ae
xweiba <lh1738289913@gmail.com>
2018-04-18 08:25:10 +0800

    添加编译

--- src/main/java/com/jnshu/taskone/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/jnshu/taskone/serviceImpl/UserServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service("userServiceImpl")
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
     @Autowired
     UserDao userDao;
 
